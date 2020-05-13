28c79330fda9c3b0c8cfec2238c93b93e9d2ae36
soegi12 <sugi12.sw@gmail.com>
2020-05-13 23:29:25 +0700

    Finish

--- app/src/main/java/com/example/sugiwiranto/FriendList.kt
+++ app/src/main/java/com/example/sugiwiranto/FriendList.kt
@@ -8,18 +8,12 @@ package com.example.sugiwiranto
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import kotlinx.android.synthetic.main.activity_walkt.*
-import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.friend_list.*
 
 class FriendList :AppCompatActivity () {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.friend_list)
@@ -28,6 +22,7 @@ class FriendList :AppCompatActivity () {
             val intent = Intent(this, AddFriend::class.java)
             startActivity(intent)
         }
+
         backfriend.setOnClickListener() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
